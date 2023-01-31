db84d10cc43025597341ab24ed41b8fc8b57dcc3
tmeson <tmeson2@gmail.com>
2023-01-31 16:17:24 -0500

    Clean up

--- FlipaClipInterviewProject/app/src/main/java/com/flipaclip/interview/data/Brush.kt
+++ FlipaClipInterviewProject/app/src/main/java/com/flipaclip/interview/data/Brush.kt
@@ -1,9 +1,3 @@
 package com.flipaclip.interview.data
 
-data class Brush(val id: String, val name: String, var strokeSize: Int) {
-    companion object {
-        const val BRUSH_ID_PEN = "brush.id.pen"
-        const val BRUSH_ID_AIRBRUSH = "brush.id.airbrush"
-        const val BRUSH_ID_SKETCH = "brush.id.sketch"
-    }
-}
+data class Brush(val id: String, val name: String, var strokeSize: Int)
--- FlipaClipInterviewProject/app/src/main/java/com/flipaclip/interview/data/BrushRepository.kt
+++ FlipaClipInterviewProject/app/src/main/java/com/flipaclip/interview/data/BrushRepository.kt
@@ -4,30 +4,20 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import java.util.concurrent.Flow
 
 class BrushRepository {
-    private var brushes = listOf(
+    var brushesFlow = MutableStateFlow(listOf(
         Brush("brush.id.pen", "Pen", 20),
         Brush("brush.id.airbrush", "Airbrush", 40),
         Brush("brush.id.sketch", "Sketch", 12)
-    )
-        private set(value) {
-            brushesFlow.value = value
-            field = value
-        }
-
-    val brushesFlow = MutableStateFlow(brushes)
-    val selectedBrushFlow = MutableStateFlow(brushes.first().id)
+    ))
+    var selectedBrush = brushesFlow.value.first().id
 
     companion object {
         val instance = BrushRepository()
     }
 
-    fun selectBrush(id: String) {
-        selectedBrushFlow.value = id
-    }
-
     fun setStrokeSize(id: String, size: Int) {
-        val newBrushList = brushes.map { it.copy() }
+        val newBrushList = brushesFlow.value.map { it.copy() }
         newBrushList.find { it.id == id }?.strokeSize = size
-        brushes = newBrushList
+        brushesFlow.value = newBrushList
     }
 }
